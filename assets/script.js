2db192827c5b9743eb1b59d19c1e615d04ae5842
Neoalina <neo-alina@yandex.ru>
2022-02-19 20:07:03 +0300

    Added class with static methods

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,52 @@ function getB(){
     return +b;
 }
 
-function plus(){
-    document.getElementById('answer').value=(getA() + getB());
+// function plus(){
+//     document.getElementById('answer').value=(getA() + getB());
+// }
+// function minus(){
+//     document.getElementById('answer').value=(getA() - getB() );
+// }
+// function multiplication(){
+//     document.getElementById('answer').value=( getA() * getB() );
+// }
+// function division(){
+//     if (getB()==0) {
+//         alert( 'Делить на ноль нельзя!' );
+//     } else {
+//     document.getElementById('answer').value=( getA() / getB() );}
+// }
+
+
+//создание класса со статическими методами
+class Calculator {
+    static PlusS(a, b) {
+        return a+b;
+    }
+    static MinusS(a, b) {
+        return a-b;
+    }
+    static MultiplicationS(a, b) {
+        return a*b;
+    }
+    static DivisionS(a, b) {
+        if (b==0) {
+            alert( 'Делить на ноль нельзя!' );
+        } else {
+        return a/b
+        }
+    }
 }
-function minus(){
-    document.getElementById('answer').value=(getA() - getB() );
+
+
+function plus() {
+    document.getElementById('answer').value=Calculator.PlusS(getA(), getB());
 }
-function multiplication(){
-    document.getElementById('answer').value=( getA() * getB() );
+function minus() {
+    document.getElementById('answer').value=Calculator.MinusS(getA(), getB());
 }
-function division(){
-    if (getB()==0) {
-        alert( 'Делить на ноль нельзя!' );
-    } else {
-    document.getElementById('answer').value=( getA() / getB() );}
+function multiplication() {
+    document.getElementById('answer').value=Calculator.MultiplicationS(getA(), getB());
 }
+function division() {
+    document.getElementById('answer').value=Calculator.DivisionS(getA(), getB());}
